2a20ad84a48d1f262dc2cffd9479c437546f867c
Kosta Harlan <kostajh@gmail.com>
2012-04-02 15:55:22 -0400

    Issue #1507322: Note that drush php-script runs your php script in a fully-bootstrapped environment

--- example.tasks.php
+++ example.tasks.php
@@ -22,15 +22,11 @@ $prod_name = '@' . $prod_record['#name'];
 $stage_record = drush_sitealias_get_record('@' . $self_record['#group'] . '.stage');
 $stage_name = '@' . $stage_record['#name'];
 
-// Rsync files from production to local
+// Rsync files from staging to local
+drush_invoke_process('@self', 'rsync', array($stage_name, $self_name), array('verbose'), array('interactive'));
 
-$cmd = "drush rsync $prod_name $self_name --verbose";
-drush_shell_exec_interactive($cmd);
-
-// Copy the database from production to local
-
-$cmd = "drush sql-sync $prod_name $self_name --verbose";
-drush_shell_exec_interactive($cmd);
+// Copy the database from staging to local
+drush_invoke_process('@self', 'sql-sync', array($stage_name, $self_name), array('verbose'), array('interactive'));
 
 // Disable modules
 
@@ -38,9 +34,7 @@ $modules = array(
   'googleanalytics',
 );
 
-foreach ($modules as $module) {
-  drush_shell_exec("drush pm-disable $module -y");
-}
+module_disable($modules);
 
 // Enable modules
 
@@ -48,14 +42,15 @@ $modules = array(
   'devel',
 );
 
-foreach ($modules as $module) {
-  drush_shell_exec("drush en $module -y");
-}
+module_enable($modules);
 
 // Disable caching
 
-drush_shell_exec("drush $self_name vset preprocess_js 0");
-drush_shell_exec("drush $self_name vset cache 0");
-drush_shell_exec("drush $self_name vset preprocess_css 0");
+variable_set('preprocess_js', 0);
+variable_set('cache', 0);
+variable_set('preprocess_css',0);
+
+// Set site name
+variable_set('site_name', 'Dev Environment');
 
 drush_log('Rebuild complete', 'success');
