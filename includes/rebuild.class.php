2ea75552bd22b2bc92a48861aacf18b287330dcc
Kosta Harlan <kostajh@gmail.com>
2013-04-18 12:51:38 -0400

    Show alternate method for defining modules to enable

--- includes/rebuild.class.php
+++ includes/rebuild.class.php
@@ -222,7 +222,7 @@ class DrushRebuild {
     }
     else {
       // Check if file is YAML format.
-      if (!class_exists('Parser')) {
+      if (!file_exists('vendor/autoload.php')) {
         return drush_set_error(dt('Could not load the config file.'));
       }
       $yaml = new Parser();
