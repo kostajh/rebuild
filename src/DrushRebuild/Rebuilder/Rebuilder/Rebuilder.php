79029bac486b39219c3ac2401c69ebdcca866a1f
Kosta Harlan <kostajh@gmail.com>
2014-03-17 16:25:54 -0400

    Move stuff around

--- src/DrushRebuild/Rebuilder/Rebuilder/Rebuilder.php
+++ src/DrushRebuild/Rebuilder/Rebuilder/Rebuilder.php
@@ -6,8 +6,23 @@
  */
 
 interface DrushRebuilder {
+  /**
+   * Constructor.
+   */
   public function __construct(array $config, array $environment, array $options = array());
+
+  /**
+   * The command callback to pass to DrushInvokeProcess().
+   */
   public function command();
+
+  /**
+   * The message to log before starting.
+   */
   public function startMessage();
+
+  /**
+   * The message to log when finished.
+   */
   public function completionMessage();
 }
