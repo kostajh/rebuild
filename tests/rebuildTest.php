5b613a20b829a7dbb0b454a0899d9811091ca7a4
Kosta Harlan <kostajh@gmail.com>
2014-04-11 14:20:47 -0400

    Clean up rebuildTest

--- tests/rebuildTest.php
+++ tests/rebuildTest.php
@@ -19,162 +19,28 @@
  */
 class RebuildTestCase extends Drush_CommandTestCase {
 
-  /**
-   * Gets the current users home directory.
-   */
+  protected function getTestsDir() {
+    return $this->getHomeDir() . '/.drush/rebuild/tests';
+  }
+
   protected function getHomeDir() {
     return $_SERVER['HOME'];
   }
 
-  /**
-   * Get the test directory.
-   */
-  protected function getTestsDir() {
-    return $this->getHomeDir() . '/.drush/drush_rebuild_tests';
-  }
-
-  /**
-   * Get a predefined set of aliases for our tests.
-   *
-   * @return array
-   *   An array of aliases.
-   */
-  protected function getAliases() {
-    return array(
-      'dev' => array(
-        'root' => $this->getTestsDir() . '/dev',
-        'uri' => 'http://dev.drush.rebuild',
-        'db-url' => 'mysql://root@localhost/drebuild_dev',
-        'path-aliases' => array(
-          '%rebuild' => $this->getTestsDir() . '/rebuild.yaml',
-          '%files' => 'sites/default/files',
-        ),
-        '#rebuild' => array(
-          'email' => 'me@example.com',
-        ),
-      ),
-      'prod' => array(
-        'root' => $this->getTestsDir() . '/prod',
-        'uri' => 'http://prod.drush.rebuild',
-        'db-url' => 'mysql://root@localhost/drebuild_prod',
-        'path-aliases' => array(
-          '%files' => 'sites/default/files',
-        ),
-        '#rebuild' => array(
-          'email' => 'me@example.com',
-        ),
-      ),
-    );
-  }
-
-  /**
-   * Prepare the working directory for our tests.
-   */
-  protected function prepareWorkingDir() {
-    if (file_exists($this->getHomeDir() . '/.drush/drush_rebuild_tests')) {
-      unish_file_delete_recursive($this->getHomeDir() . '/.drush/drush_rebuild_tests');
-    }
-    mkdir($this->getHomeDir() . '/.drush/drush_rebuild_tests');
-  }
-
-  /**
-   * Copy the predefined aliases into the working directory.
-   */
-  protected function copyAliases() {
-    $records = array();
-    touch($this->getTestsDir() . '/drebuild.aliases.drushrc.php');
-    foreach ($this->getAliases() as $name => $alias) {
-      $records[] = sprintf('$aliases[\'%s\'] = %s;', $name, var_export($alias, TRUE));
-    }
-    $contents = "<?php\n\n" . implode("\n\n", $records);
-    file_put_contents($this->getTestsDir() . '/drebuild.aliases.drushrc.php', $contents);
-  }
-
-  /**
-   * Copy the config to the working dir.
-   */
-  protected function copyConfig() {
-    if (file_exists($this->getTestsDir() . '/rebuild.yaml')) {
-      drupal_unlink($this->getTestsDir() . '/rebuild.yaml');
-    }
-    copy($this->getHomeDir() . '/.drush/rebuild/tests/rebuild.yaml', $this->getTestsDir() . '/rebuild.yaml');
-
-  }
-
-  /**
-   * Copy the overrides to the working dir.
-   */
-  protected function copyOverrides() {
-    if (file_exists($this->getTestsDir() . '/local.rebuild.yaml')) {
-      drupal_unlink($this->getTestsDir() . '/local.rebuild.yaml');
-    }
-    copy($this->getHomeDir() . '/.drush/rebuild/tests/local.rebuild.yaml', $this->getTestsDir() . '/local.rebuild.yaml');
-  }
-
-  /**
-   * Install test sites.
-   */
   protected function installTestSites() {
     $options = array(
-      'site-name' => 'Prod',
-      'alias-path' => $this->getTestsDir(),
+      'site-name' => 'Dev',
+      'alias-path' => $this->getTestsDir() . '/fixtures',
       'yes' => TRUE,
       'quiet' => TRUE,
     );
-    if (!file_exists($this->getTestsDir() . '/prod')) {
-      mkdir($this->getTestsDir() . '/prod');
-    }
-    else {
-      unish_file_delete_recursive($this->getTestsDir() . '/prod');
-    }
-    if (!file_exists($this->getTestsDir() . '/dev')) {
-      mkdir($this->getTestsDir() . '/dev');
-    }
-    else {
-      unish_file_delete_recursive($this->getTestsDir() . '/dev');
-    }
-    // Install prod site.
-    $this->drush('dl', array('drupal'), array(
-      'drupal-project-rename' => 'prod',
-      'destination' => $this->getTestsDir(),
-      'cache' => TRUE,
-      'yes' => TRUE,
-      'quiet' => TRUE)
-    );
-    $this->drush('site-install', array('minimal'), $options, '@drebuild.prod');
-    $this->log('Installed prod site');
-    // Install dev site.
-    $this->drush('dl', array('drupal'), array(
-      'drupal-project-rename' => 'dev',
-      'destination' => $this->getTestsDir(),
-      'cache' => TRUE,
-      'yes' => TRUE,
-      'quiet' => TRUE)
-    );
-    $options['site-name'] = 'Dev';
     $this->drush('site-install', array('minimal'), $options, '@drebuild.dev');
-    $this->log('Installed dev site');
-    // Check that the name was set.
-    $this->drush('variable-get', array('site_name'), array('alias-path' => $this->getTestsDir(), 'format' => 'json'), '@drebuild.dev');
-    $this->assertEquals('{"site_name":"Dev"}', $this->getOutput());
-    $this->drush('variable-get', array('site_name'), array('alias-path' => $this->getTestsDir(), 'format' => 'json'), '@drebuild.prod');
-    $this->assertEquals('{"site_name":"Prod"}', $this->getOutput());
-    // Add a file to sites/default/files in @prod
-    file_put_contents($this->getTestsDir() . '/prod/sites/default/files/hello.world', 'Hello world!');
   }
 
   /**
    * Test a basic rebuild.
    */
   public function testRebuild() {
-    // Make an alias for the dev/prod sites.
-    $this->prepareWorkingDir();
-    $this->copyAliases();
-    // Copy test rebuild file.
-    $this->copyConfig();
-    // Copy overrides file.
-    $this->copyOverrides();
-
     // Clear drush cache before running tests.
     $this->drush('cc', array('drush'));
     // @todo Copy test scripts.
@@ -185,7 +51,8 @@ class RebuildTestCase extends Drush_CommandTestCase {
     $this->drush('env-rebuild', array(),
       array(
         'include' => $this->getHomeDir() . '/.drush/rebuild',
-        'alias-path' => $this->getTestsDir(), 'debug' => TRUE,
+        'alias-path' => $this->getTestsDir(),
+        'debug' => TRUE,
         'source' => '@drebuild.prod',
         'yes' => TRUE,
       ),
@@ -268,7 +135,6 @@ class RebuildTestCase extends Drush_CommandTestCase {
    * Tests the site install rebuild.
    */
   public function testSiteInstall() {
-    copy($this->getHomeDir() . '/.drush/rebuild/tests/site_install.rebuild.yaml', $this->getTestsDir() . '/rebuild.yaml');
     // Run the rebuild.
     $this->drush('env-rebuild', array(''),
       array(
